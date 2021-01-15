edb653a400872d8ac8db95e563e4cac9076e9984
ccguself <ccguself@163.com>
2021-01-15 16:52:57 +0800

    Initiate the classes

--- HfqSelf/TradeLogAnalysis.py
+++ HfqSelf/TradeLogAnalysis.py
@@ -2,3 +2,18 @@ import os
 import numpy as np
 import pandas as pd
 import seaborn as sns
+
+
+class SignalLog:
+    def __init__(self):
+        pass
+
+
+class MarketLog:
+    def __init__(self):
+        pass
+
+
+class AnalysisLog:
+    def __init__(self):
+        pass
