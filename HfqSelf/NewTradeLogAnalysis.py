48d9c42588feeefc42d53bf19323fb71a58ea1f1
ccguself <ccguself@163.com>
2021-01-20 13:57:34 +0800

    完善评价指标

--- HfqSelf/NewTradeLogAnalysis.py
+++ HfqSelf/NewTradeLogAnalysis.py
@@ -5,36 +5,55 @@ import seaborn as sns
 import datetime
 from joblib import Parallel, delayed
 from peewee import *
-
 from prophet.predict import signal_output
 
 
 class SingleCombinedLog:
-    def __init__(self, raw_signal, df_predict_interval):
+    def __init__(
+        self,
+        raw_signal,
+        df_predict_interval,
+        strategy_name="NN",
+        direction="Long",
+        duration_minute=2,
+        pct_predict=2,
+    ):
+        self.strategy_name = strategy_name
+        self.direction = direction
+        self.duration_minute = duration_minute
+        self.pct_predict = pct_predict
         self.raw_signal = raw_signal
         self.df_predict_interval = df_predict_interval
-        self.strategy_name = "Parent"
-        self.direction = "Parent"
-        self.duration_minute = "Parent"
+        self._check_signal()
 
     def _check_signal(self):
-        pass
+        interval_begin = self.df_predict_interval["last_price"].iloc[0]
+        interval_max = self.df_predict_interval["last_price"].max()
+        interval_min = self.df_predict_interval["last_price"].min()
+        if self.direction == "Long":
+            if (interval_begin + self.pct_predict) < interval_max:
+                if self.raw_signal == None:
+                    self.correct = "FT"
+                else:
+                    self.correct = "TT"
+            else:
+                if self.raw_signal == None:
+                    self.correct = "FF"
+                else:
+                    self.correct = "TF"
+        elif self.direction == "Short":
+            if (interval_begin - self.pct_predict) > interval_min:
+                if self.raw_signal == None:
+                    self.correct = "FT"
+                else:
+                    self.correct = "TT"
+            else:
+                if self.raw_signal == None:
+                    self.correct = "FF"
+                else:
+                    self.correct = "TF"
 
-
-class NNLongTwoLog(SingleCombinedLog):
-    def __init__(self, raw_signal, df_predict_interval):
-        super().__init__(raw_signal, df_predict_interval)
-        self.strategy_name = "NN"
-        self.direction = "Long"
-        self.duration_minute = "Two"
-
-
-class NNShortTwoLog(SingleCombinedLog):
-    def __init__(self, raw_signal, df_predict_interval):
-        super().__init__(raw_signal, df_predict_interval)
-        self.strategy_name = "NN"
-        self.direction = "Short"
-        self.duration_minute = "Two"
+        return self.correct
 
 
 class MarketLog:
@@ -78,6 +97,38 @@ class MarketLog:
         return self.df_market
 
 
+class AnalysisSignal:
+    def __init__(self, analysised_signal_dict):
+        self.analysised_signal_dict = analysised_signal_dict
+
+    def count_signal(self):
+        signal_lst = list(self.analysised_signal_dict.values())
+        valid_signal_lst = [x for x in signal_lst if x.raw_signal != None]
+        self.num_signal_total = len(valid_signal_lst)
+        return self.num_signal_total
+
+    def cal_acc(self):
+        signal_lst = list(self.analysised_signal_dict.values())
+        num_tf = len([x for x in signal_lst if x.correct == "TF"])
+        num_tt = len([x for x in signal_lst if x.correct == "TT"])
+        num_ft = len([x for x in signal_lst if x.correct == "FT"])
+        num_ff = len([x for x in signal_lst if x.correct == "FF"])
+        self.acc = num_tt / (num_tf + num_tt)
+        return self.acc
+
+    def cal_recall(self):
+        signal_lst = list(self.analysised_signal_dict.values())
+        num_tf = len([x for x in signal_lst if x.correct == "TF"])
+        num_tt = len([x for x in signal_lst if x.correct == "TT"])
+        num_ft = len([x for x in signal_lst if x.correct == "FT"])
+        num_ff = len([x for x in signal_lst if x.correct == "FF"])
+        self.recall = num_tt / (num_tt + num_ft)
+        return self.recall
+
+    def generate_order(self):
+        pass
+
+
 class AnalysisLog:
     """一些注意点：
     1. dict_...会分别记录各个类型的信号，不同模型发出的，不同有效时段的，不同方向的
@@ -91,6 +142,7 @@ class AnalysisLog:
         self.frequency_predict = frequency_predict
         self.dict_nn_long_two = {}  # datetime: SingleCombinedLog
         self.dict_nn_short_two = {}  # datetime: SingleCombinedLog
+        # self.result_analysis =
         self.model = signal_output()
 
     def generate_single_combined_log(self):
@@ -127,21 +179,41 @@ class AnalysisLog:
                 ## 2分钟的信号
                 # lst_signal_nn_long_two
                 if len(lst_signal_nn_long_two) == 0:
-                    self.dict_nn_long_two[timestamp_beging] = NNLongTwoLog(
-                        None, df_predict
+                    self.dict_nn_long_two[timestamp_beging] = SingleCombinedLog(
+                        strategy_name="NN",
+                        direction="Long",
+                        duration_minute=2,
+                        pct_predict=2,
+                        raw_signal=None,
+                        df_predict_interval=df_predict,
                     )
                 else:
                     signal = lst_signal_nn_long_two[0]
-                    self.dict_nn_long_two[timestamp_beging] = NNLongTwoLog(
-                        signal, df_predict
+                    self.dict_nn_long_two[timestamp_beging] = SingleCombinedLog(
+                        strategy_name="NN",
+                        direction="Long",
+                        duration_minute=2,
+                        pct_predict=2,
+                        raw_signal=signal,
+                        df_predict_interval=df_predict,
                     )
                 # lst_signal_nn_short_two
                 if len(lst_signal_nn_short_two) == 0:
-                    self.dict_nn_short_two[timestamp_beging] = NNLongTwoLog(
-                        None, df_predict
+                    self.dict_nn_short_two[timestamp_beging] = SingleCombinedLog(
+                        strategy_name="NN",
+                        direction="Short",
+                        duration_minute=2,
+                        pct_predict=2,
+                        raw_signal=None,
+                        df_predict_interval=df_predict,
                     )
                 else:
                     signal = lst_signal_nn_short_two[0]
-                    self.dict_nn_short_two[timestamp_beging] = NNLongTwoLog(
-                        signal, df_predict
+                    self.dict_nn_short_two[timestamp_beging] = SingleCombinedLog(
+                        strategy_name="NN",
+                        direction="Short",
+                        duration_minute=2,
+                        pct_predict=2,
+                        raw_signal=signal,
+                        df_predict_interval=df_predict,
                     )
