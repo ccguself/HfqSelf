0324ab91f696e8321973d6670322f81447edac6d
ccguself <ccguself@163.com>
2021-02-17 18:47:06 +0800

    添加回测模块

---
+++ HfqSelf/BTC/read_data.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import os
+import torch
+from peewee import *
+from joblib import Parallel, delayed
+from sklearn.pipeline import Pipeline
--- HfqSelf/faker/feature.py
+++ HfqSelf/faker/feature.py
@@ -2,7 +2,10 @@ import os
 import pytorch
 import numpy as np
 import pandas as pd
-from .data import RawData
+from data import RawData
+from joblib import Parallel, delayed
+from config import data_config
+import datetime
 
 # 完成因子筛选（纵），样本集的生成（横）
 
@@ -12,9 +15,22 @@ class FactorData:
         self.raw_data = raw_data
 
     # resample时，需要对10：15，11：30这些数据，做特殊处理：在resample之前，截取时候需要跨过休市时间；resample过后，要dropna
-    def load_factor_zoo_artificial(self, n_job):
-        def load_factor_zoo_artificial_utils():
-            # step1.
+    def resample_and_load_factor_zoo_artificial(self, n_job):
+        def resample_and_load_factor_zoo_artificial_utils(
+            X, timestamp, config=data_config
+        ):
+            # step1. 向“过去”截取用于训练的时间区间
+            interval_begin = timestamp - datetime.timedelta(
+                seconds=config["window_resample_second"]
+            )
+            interval_end = timestamp
+
+            X["datetime"]
             return
+
+        data_slice_factors_added_lst = Parallel(n_jobs=n_job)(
+            delayed(resample_and_load_factor_zoo_artificial_utils)(datetime_slice)
+            for datetime_slice in datetime_sampled_series_lst
+        )
 
         return
