a1def8546de9306513cc3a3d53b39ea430de448c
ccguself <ccguself@163.com>
2021-02-17 20:51:02 +0800

    回测模块框架

---
+++ HfqSelf/BackTest/backtesting.py
@@ -0,0 +1,76 @@
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+class ModelInfo:
+    def __init__(self, name_model, window_train, window_predict):
+        self.name_model = name_model
+        self.window_train = window_train
+        self.window_predict = window_predict
+
+    def _load_model(self):
+        # 加载模型
+        # self.model =
+        return
+
+
+class DailyLog:
+    def __init__(self, date, market_data, signal_timestamp_lst, signal_duration):
+        self.date = date
+        self.market_data = market_data
+        self.signal_timestamp_lst = signal_timestamp_lst
+        self._cal_profit()
+        self._cal_acc()
+        self._cal_reacall()
+
+    def _cal_profit(self):
+        def _cal_profit_utils():
+            # 添加设置滑点的参数
+            return
+
+        return
+
+    def _cal_acc(self):
+        def _cal_acc_utils():
+            return
+
+        return
+
+    def _cal_reacall(self):
+        def _cal_recall_utils():
+            return
+
+        return
+
+
+class MarketData:
+    def __init__(self, path_data, date_begin, date_end):
+        self.path_data = path_data
+        self.date_begin = date_begin
+        self.date_end = date_end
+        self.file_paths = []
+
+    def _get_trading_dates(self):
+        names = list(
+            filter(
+                lambda x: x >= self.date_begin and x <= self.date_end,
+                os.listdir(self.path_data),
+            )
+        )
+        self.file_paths.extend([os.path.join(self.path_data, name) for name in names])
+        self.file_paths.sort()
+
+    # 只获取信号的时间戳
+    def generate_signal(self, n_job=4):
+        def _generate_signal_utils(
+            daily_file_path: str, model_info: ModelInfo
+        ) -> DailyLog:
+            daily_date = daily_file_path.split(".")[0][-8:]  # 截取日期
+            daily_market_data = pd.read_csv(daily_file_path)
+
+            return
+
+    # 根据信号的时间戳，计算净值
