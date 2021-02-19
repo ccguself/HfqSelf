f9b69f6207a84e296f3ae48138c7b5edf42c8d8a
ccguself <72603021+ccguself@users.noreply.github.com>
2021-02-19 13:56:46 +0800

    模型相关

--- HfqSelf/faker/config.py
+++ HfqSelf/faker/config.py
@@ -3,18 +3,18 @@ import datetime
 
 data_config = {
     "sample_mode": "tick",
-    "window_sample_second": 15,
     "window_train_minute": 10,
     "window_resample_second": 15,
+    "window_predict_minute": 15,
+    "freq_tick_per_second": 2
 }
 
-def get_trading_and_break_interval_futures(symbol="RB", date="2020-01-18"):
-    if symbol == "RB":
-        date_today = datetime.strptime(date, "%Y-%m-%d").date()
-        date_yesterday = date_today - datetime.timedelta(days=1)
-        
-        
-        interval_trading_1 = Interval()
-        interval_trading_lst =   # 用于盘间的休市时间判断
-        interval_break =   # 用于夜盘和日盘间的判断
 
+# def get_trading_and_break_interval_futures(symbol="RB", date="2020-01-18"):
+#     if symbol == "RB":
+#         date_today = datetime.strptime(date, "%Y-%m-%d").date()
+#         date_yesterday = date_today - datetime.timedelta(days=1)
+
+#         interval_trading_1 = Interval()
+interval_trading_lst =   # 用于盘间的休市时间判断
+interval_break =   # 用于夜盘和日盘间的判断
---
+++ HfqSelf/faker/label.py
@@ -0,0 +1,35 @@
+from config import data_config
+import datetime
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.base import BaseEstimator, TransformerMixin
+from joblib import Parallel, delayed
+
+
+class GetLabel(BaseEstimator, TransformerMixin):
+    def __init__(self, data_config, n_job):
+        self.data_config = data_config
+        self.n_job = n_job
+
+    def fit(self, X, y=None):
+        return self  # nothing else to do
+
+    def transform(self, X):
+        def transform_utils(time_index, signal_duration_min, df):
+            label_dict = {}
+            interval_min = datetime.timedelta(minutes=signal_duration_min)
+            df_interval = df.loc[time_index:time_index + interval_min, :]
+
+            # label_1 : pct of Mid Price
+            label_dict["label_midprice_pct"] = df_interval["mid_price"].iloc[-1] / \
+                df_interval["mid_price"].iloc[0]
+
+            return label_dict
+
+        label_dict_lst = Parallel(n_jobs=self.n_job)(
+            delayed(transform_utils)(
+                time_index, self.data_config["window_predict_minute"], X)
+            for time_index in list(X.index)
+        )
+
+        return X
